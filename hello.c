e60b5b7ec8a8fcdbb87cd0480c2005631c0d6ea0
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-01-18 19:12:08 +0300

    reading first chapter

--- hello.c
+++ hello.c
@@ -1,6 +1,16 @@
-#include <stdio.h>
+#include <stdio.h> // include the standard input/output library
 
-int main(){
-    printf("hello world!\n");
+// a C program consists of functions and variables
+int sub(){ // function
+           // expects no arguments, empty parentheses
+    return 5;
+}
+
+int main(){ // main function
+            // the program begins executing at the beginning of main
+    printf("hello world!\n"); // another function, this time from stdio.h
+    int i = sub(); // variable
+                   // i is an int, is the result of sub()
+    printf("result of sub is %d\n", i);
     return 0;
 }
