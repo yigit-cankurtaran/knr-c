59dd2ac87aff191c71ff0956f7c7c532876214cf
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-01-18 19:22:12 +0300

    ctof numbers are floats instead of ints

--- ctof.c
+++ ctof.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 int main(){
-    int fahr, celsius;
-    int lower, upper, step;
+    float fahr, celsius; //units, doing them as float
+    int lower, upper, step; //limits + how much we're gonna raise
 
-    lower = 0;
-    upper = 100;
-    step = 1;
+    lower = 0; //freezing
+    upper = 100; //boiling
+    step = 1; //1 degree each time
 
     celsius = lower;
-    while(celsius <= upper){
+    while(celsius <= upper){ //while we're below freezing
         //celsius = 5 * (fahr - 32) / 9;
-        fahr = (9 * celsius / 5) + 32;
-        printf("%d\t%d\n", celsius, fahr);
-        celsius = celsius + step;
+        fahr = (9 * celsius / 5) + 32; //p much just reversed the formula
+        printf("%.2f\t%f\n", celsius, fahr); //both floats, c only has the last 2 digits
+        celsius = celsius + step; //raise the number
     }
 }
 
