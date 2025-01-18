cf1258a26973ee48faac05b345bfe966f3e63404
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-01-19 01:59:04 +0300

    finished the temp conversion exercises

--- ctof.c
+++ ctof.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
+#define LOWER 0 //freezing
+#define UPPER 100 //boiling
+#define STEP 1 //1 degree each time
+               //no = or ; or anything just define the thing
+               //this is a macro not a statement
+
 int main(){
     float fahr, celsius; //units, doing them as float
-    int lower, upper, step; //limits + how much we're gonna raise
+    celsius = LOWER;
 
-    lower = 0; //freezing
-    upper = 100; //boiling
-    step = 1; //1 degree each time
-
-    celsius = lower;
-    while(celsius <= upper){ //while we're below freezing
+    printf("FAHRENHEIT\tCELSIUS\n");
+    while(celsius <= UPPER){ //while we're below freezing
         //celsius = 5 * (fahr - 32) / 9;
         fahr = (9 * celsius / 5) + 32; //p much just reversed the formula
         printf("%3.2f\t%6.f\n", celsius, fahr); //both floats, c only has the last 2 digits
-        celsius = celsius + step; //raise the number
+        celsius = celsius + STEP; //raise the number
     }
 }
--- ftoc-for.c
+++ ftoc-for.c
@@ -7,14 +7,13 @@
 int main(){
     int fahr, celsius;
 
-    fahr = LOWER;
-    // heading for exercise 1.3
     printf("FAHRENHEIT\tCELSIUS\n");
-    while(fahr <= UPPER){
+    for(fahr = UPPER; fahr >= LOWER; fahr -= STEP){
+        // fahr = upper, check if fahr >= lower, if so run fahr -= step, run it again
+        // if fahr !>= lower, stop
         celsius = 5 * (fahr - 32) / 9;
         printf("%3d\t%12d\n", fahr, celsius); //little bit of styling
                                               //right center, first is 3, 2nd is 12 digits long
-        fahr += STEP;
     }
 }
 
