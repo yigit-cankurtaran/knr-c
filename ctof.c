e5acde43761e6434b6639aa9cf53f9e33321881c
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-01-18 19:49:48 +0300

    finished exercises 1.3 and 1.4

--- ctof.c
+++ ctof.c
@@ -12,8 +12,7 @@ int main(){
     while(celsius <= upper){ //while we're below freezing
         //celsius = 5 * (fahr - 32) / 9;
         fahr = (9 * celsius / 5) + 32; //p much just reversed the formula
-        printf("%.2f\t%f\n", celsius, fahr); //both floats, c only has the last 2 digits
+        printf("%3.2f\t%6.f\n", celsius, fahr); //both floats, c only has the last 2 digits
         celsius = celsius + step; //raise the number
     }
 }
-
--- ftoc.c
+++ ftoc.c
@@ -9,9 +9,12 @@ int main(){
     step = 20;
 
     fahr = lower;
+    // heading for exercise 1.3
+    printf("FAHRENHEIT\tCELSIUS\n");
     while(fahr <= upper){
         celsius = 5 * (fahr - 32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        printf("%3d\t%12d\n", fahr, celsius); //little bit of styling
+                                              //right center, first is 3, 2nd is 12 digits long
         fahr = fahr + step;
     }
 }
