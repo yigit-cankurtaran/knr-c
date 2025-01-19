1a070bc46f863b2a9df24bb34dc900c9973e15a5
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-01-19 17:33:53 +0300

    finished exercise 1.8

--- char_exercises.c
+++ char_exercises.c
@@ -1,3 +1,20 @@
 #include <stdio.h>
 
-void exercise
+void exercise18(){
+    int input;
+    int tabs = 0, spaces = 0, newlines = 0;
+
+    while ((input = getchar()) != EOF){
+        if (input == '\t') ++tabs; // '' = char constant "" = string literal
+                                   // we need char constants here bc we want 1 char
+        else if (input == '\n') ++newlines;
+        else if (input == ' ') ++spaces;
+    }
+
+    printf("tabs: %d, spaces: %d, newlines: %d\n", tabs, spaces, newlines);
+}
+
+int main(){
+    exercise18();
+    return 1;
+}
