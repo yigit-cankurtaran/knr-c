eb2691e76d3985b36a4c16c8b27758802fb013ed
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-01-19 16:58:50 +0300

    better while loop

--- chars.c
+++ chars.c
@@ -6,7 +6,7 @@ void chars() { // void = doesn't return anything
     c = getchar(); // gets the first char from whatever we type in
     printf("the char we got is: ");
     d = putchar(c); // prints what getchar gets. e.g. "h" from "hello"
-    printf("\nthe ascii value we got is: %d\n", d);
+    printf("\nthe ascii value is: %d\n", d);
     // prints the ASCII value of "h" from "hello" (104)
 }
 
@@ -16,17 +16,20 @@ void input_copy(){
                // and getchar() returns an int
                // if we declared this as a char it couldn't handle EOF
 
-    input = getchar(); // get the first char
+    //input = getchar(); // get the first char
                        // not really necessary in this context
                        // but it's good practice to initialize the loop with the first input
 
-    while (input != EOF){ // while we don't hit EOF
+    // we can also initialize in the while statement itself
+    // while (input != EOF){ // while we don't hit EOF
                           // e.g. before we hit enter
+    while ((input = getchar()) != EOF){
         putchar(input); // print the input
                         // prints when we press enter due to terminal buffering
                         // enter flushes the line buffer and makes the input available
                         // until then the program doesn't know we typed anything
-        input = getchar(); // keep reading the next char until we signal EOF
+        // input = getchar(); // keep reading the next char until we signal EOF
+        // commenting out since we already have it in the loop statement
     }
 }
 
