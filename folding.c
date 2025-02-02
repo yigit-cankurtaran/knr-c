b9b0fc2f33bde671ab1cfeba7704d40361bf1bf9
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-02-02 15:28:17 +0300

    wrote another folding, like the old one better

--- folding.c
+++ folding.c
@@ -9,18 +9,19 @@
 */
 
 // simple solution, might write a better one
+// more complicated one is in folding_buffer but i like this one better
 int main(){
-    int c, len = 0, pos = 0;
+    int c, len = 0, pos = 0; // i can use pos to write one that only breaks on spaces and tabs
 
     while((c = getchar()) != EOF){
         if (c != '\n' && c != '\0'){
             len++;
-            pos++;
-        } if (len > N){
+        }
+        if (len > N){
             c = '\n';
             len = 0; // resetting len because len needs to update after every wrap
                      // or we get a newline for every char instead of the chars themselves.
-            pos = 0;
+            pos = 0; // reset position
         }
         putchar(c);
     }
---
+++ folding_buffer.c
@@ -0,0 +1,63 @@
+// complicated approach (with buffer)
+int main(){
+    int c, last_space = -1; // will use c as input and last_space to remember last space position
+    int len = 0, pos = 0; // length and position
+    char myString[10 * N]; // buffer approach.
+
+    while((c = getchar()) != EOF){
+        if (c != '\n' && c != '\0'){
+            myString[pos] = c;
+            if (myString[pos] == ' ' || myString[pos] == '\t'){
+                last_space = pos;
+            }
+            len++;
+            pos++;
+            myString[pos] = '\0'; // adding null termination
+
+            if (len > N) {
+                if (last_space > 0) {
+                    // print up to last space
+                    myString[last_space] = '\0';
+                    printf("%s\n", myString);
+
+                    // move remaining chars to the start of buffer
+                    int i, j;
+                    for (i = last_space + 1, j = 0; i < pos; i++, j++){
+                        // i starts right after the space
+                        // j starts at the beginning of the buffer
+                        // i < pos keeps going until we hit the end of text
+                        // move both counters forward
+                        myString[j] = myString[i];
+                    }
+                    myString[j] = '\0';
+
+                    // resetting counters
+                    pos = j;
+                    len = j;
+                    last_space = -1;
+                } else {
+                    // no space exists
+                    myString[pos - 1] = '\0';
+                    printf("%s\n", myString);
+                    pos = 0;
+                    len = 0;
+                    last_space = -1;
+                }
+            } else if (c == '\n') {
+                // print current buffer and reset
+                myString[pos] = '\0';
+                printf("%s\n", myString);
+                pos = 0;
+                len = 0;
+                last_space = -1;
+            }
+        }
+    }
+    // if any text remains
+    if (pos > 0) {
+        myString[pos] = '\0';
+        printf("%s", myString);
+    }
+    
+    return 0;
+}
