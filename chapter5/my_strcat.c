966de399a9551f4513a6b1091957f005eb02200f
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-02-05 16:47:34 +0300

    finished exercise 5-3

---
+++ chapter5/my_strcat.c
@@ -0,0 +1,37 @@
+/* Exercise 5-3. Write a pointer version of the function strcat that we showed
+in Chapter 2: strcat(s, t) copies the string t to the end of s. */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* my_strcat(char* s, char* t){
+    int s_length = strlen(s);
+    int t_length = strlen(t);
+    int length = s_length + t_length;
+    char* new_string = (char*) malloc((length * sizeof(char)) + 1);
+    // +1 bc we want a /0 at the end of string
+
+    int i;
+
+    // add s to new string
+    for (i = 0; i < s_length; i++) {
+        *(new_string + i) = *(s + i);
+    }
+
+    // add t to new string
+    for (int j = 0; j < t_length; j++) {
+        *(new_string + s_length + j) = *(t + j);
+    }
+    new_string[length] = '\0';
+
+    return new_string;
+}
+
+int main(void){
+    char hello[] = "hello";
+    char world[] = "world";
+
+    printf("new string is %s\n", my_strcat(hello, world));
+    return 0;
+}
